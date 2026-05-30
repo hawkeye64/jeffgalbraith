e49f6cd93f9caa5936e4b78c48ebdaf559331858
Jeff <galbraith64@gmail.com>
2026-05-30 05:19:27 -0600

    chore: update site title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <!-- <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header> -->
-
     <q-page-container>
       <router-view />
     </q-page-container>
